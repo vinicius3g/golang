931614e2ad5184dd1d3d8ac47472b9652c465a66
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-06-24 10:34:30 -0300

    feat: adicionadas funcionalidades de - validação - calculo de preço - validaçao de preço

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,5 +1,9 @@
 package domain
 
+import "errors"
+
+var ErrTicketPriceZero = errors.New("ticket price must be greater than zero")
+
 type TicketType = string
 
 const (
@@ -13,4 +17,22 @@ type Ticket struct {
 	Spot       *Spot
 	TicketType TicketType
 	Price      float64
+}
+
+func IsValidTycketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) ValidatePrice() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceZero
+	}
+
+	return nil
 }
