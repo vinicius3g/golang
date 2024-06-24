d42b9f34e655816ed87df94722221a08a99a4710
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-06-24 14:42:10 -0300

    feat: doc e ajustes de parametros

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,38 +1,47 @@
 package domain
 
-import "errors"
-
-var ErrTicketPriceZero = errors.New("ticket price must be greater than zero")
-
-type TicketType = string
-
-const (
-	TicketTypeHalf TicketType = "half" //half=price ticket
-	TicketTypeFull TicketType = "full" //full=price ticket
+import (
+	"errors"
 )
 
+// Errors
+var (
+	ErrInvalidTicketKind = errors.New("invalid ticket kind")
+)
+
+// TicketKind represents the kind of a ticket.
+type TicketKind string
+
+const (
+	TicketKindHalf TicketKind = "half" // Half-price ticket
+	TicketKindFull TicketKind = "full" // Full-price ticket
+)
+
+// IsValidTicketKind checks if a ticket kind is valid.
+func IsValidTicketKind(ticketKind TicketKind) bool {
+	return ticketKind == TicketKindHalf || ticketKind == TicketKindFull
+}
+
+// Ticket represents a ticket for an event.
 type Ticket struct {
 	ID         string
 	EventID    string
 	Spot       *Spot
-	TicketType TicketType
+	TicketKind TicketKind
 	Price      float64
 }
 
-func IsValidTycketType(ticketType TicketType) bool {
-	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
-}
-
+// CalculatePrice calculates the price based on the ticket kind.
 func (t *Ticket) CalculatePrice() {
-	if t.TicketType == TicketTypeHalf {
+	if t.TicketKind == TicketKindHalf {
 		t.Price /= 2
 	}
 }
 
-func (t *Ticket) ValidatePrice() error {
+// Validate checks if the ticket data is valid.
+func (t *Ticket) Validate() error {
 	if t.Price <= 0 {
-		return ErrTicketPriceZero
+		return errors.New("ticket price must be greater than zero")
 	}
-
 	return nil
 }
